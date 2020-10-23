69500b0a20caae89982e3621ba7b612f01c7513f
Ricardo Mallols <ricardo.mallols@gmail.com>
2020-10-23 16:24:37 +0100

    Init repo showing the differences on the grid depending on whether requestAnimationFrame is broadcasted or not

--- src/App.js
+++ src/App.js
@@ -1,26 +1,98 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import NewWindow from 'react-new-window';
+import { AgGridReact } from 'ag-grid-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+
+const windowNameWithRequestAnimationFrame = 'gridWindow2';
+
+const App = () => {
+    const regions = { america: 'America', europe: 'Europe' };
+    const cars = [
+        { make: "Ford", model: "Fiesta", region: regions.america },
+        { make: "Opel", model: "Corsa", region: regions.europe },
+        { make: "Dodge", model: "Viper", region: regions.america },
+        { make: "Fiat", model: "Ritmo", region: regions.europe },
+        { make: "Chevrolet", model: "Camaro", region: regions.america },
+        { make: "Seat", model: "Toledo", region: regions.europe },
+    ];
+    const [showGrid1, setShowGrid1] = useState(false);
+    const [showGrid2, setShowGrid2] = useState(false);
+    const [region, setRegion] = useState(regions.america);
+
+    const getCarsInRegion = () => cars.filter(car => car.region === region);
+
+    const registerOpenedWindow = (newWindow) => {
+        window.parent[newWindow.name] = newWindow
+    }
+
+    return (
+        <div className="ag-theme-alpine" style={{ height: 400, width: 600 }} center="screen">
+            <button onClick={() => setShowGrid1(true)}>Show grids in new window (Some filter changes won't work if the main window is not active)</button>
+            <button onClick={() => setShowGrid2(true)}>Show grids in new windows  (All filter changes will work if the main window is not active)</button>
+            {
+                showGrid1 &&
+                <Grid
+                    name="gridWindow1"
+                    cars={getCarsInRegion()}
+                    regions={regions}
+                    onRegionChange={setRegion}
+                    onWindowOpened={registerOpenedWindow}
+                />
+            }
+            {
+                showGrid2 &&
+                <Grid
+                    name={windowNameWithRequestAnimationFrame}
+                    cars={getCarsInRegion()}
+                    regions={regions}
+                    onRegionChange={setRegion}
+                    onWindowOpened={registerOpenedWindow}
+                />
+            }
+        </div>
+    );
+};
+
+const Grid = ({ name, cars, regions, onWindowOpened, onRegionChange }) => (
+    <NewWindow name={name} features={{ width: 500, height: 500 }} onOpen={onWindowOpened}>
+        <div>
+            {
+                Object.keys(regions).map(region => (
+                    <button key={region} onClick={() => onRegionChange(regions[region])}>
+                        Show cars from {region}
+                    </button>
+                ))
+            }
+        </div>
+        <AgGridReact
+            columnDefs={[
+                { headerName: 'Make', field: 'make', cellRenderer: params => `${params.value}!` },
+                { headerName: 'Model', field: 'model', valueFormatter: params => `${params.value}!` },
+                { headerName: 'Price', field: 'region' }
+            ]}
+            rowData={cars}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </AgGridReact>
+    </NewWindow>
+)
+
+function customRequestAnimationFrame(callback) {
+    let callbackReceived = false;
+    function receiveCallback(args) {
+        if (!callbackReceived) {
+            callback(args);
+        }
+        callbackReceived = true;
+    }
+    if (window[windowNameWithRequestAnimationFrame]) {
+        window[windowNameWithRequestAnimationFrame].requestAnimationFrame(receiveCallback);
+    }
+    return mainWindowRequestAnimationFrame(receiveCallback);
 }
+
+const mainWindowRequestAnimationFrame = window.requestAnimationFrame;
+window.requestAnimationFrame = customRequestAnimationFrame;
 
 export default App;
